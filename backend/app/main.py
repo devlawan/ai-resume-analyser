d79caa25ad2fd54a6841cc7e252a2eb50e7758e8
Lawan Goud <lavan.bairi@gmail.com>
2026-05-20 21:53:26 +0530

    fix: allow all vercel origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,9 +16,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ai-resume-analyser-opal.vercel.app",
-        "https://ai-resume-analyser-git-main-codedbylawan-devs-projects.vercel.app"
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
